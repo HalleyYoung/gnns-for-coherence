397731049e12f1804ea226172c5339a7b246c07b
Halley Young <halley@nichols-ve500-0636.apn.wlan.upenn.edu>
2019-04-25 17:52:42 -0400

    added nyt.sent

--- getnyt.py
+++ getnyt.py
@@ -25,13 +25,13 @@ for (fold, sub_dirs, files) in a:
 			for val in ps:
 				text = val.text
 				sent_text = nltk.sent_tokenize(text)
-				if len(sent_text) > 4 and str(sent_text[-1])[-1] == "." and all([len(i) > 20 for i in sent_text]):
+				if len(sent_text) >= 4 and str(sent_text[-1])[-1] == "." and all([len(i) > 20 and "." not in i[:10] and "LEAD" not in i[:10] for i in sent_text]):
 					print("in " + str(text_count))
 					sents.append(sent_text)
 					sent_counts[text_count] = range(sent_count, sent_count + len(sent_text))
 					sent_count = sent_count + len(sent_text)
 					text_count += 1
-					if sent_count > 20000:
+					if sent_count > 80000:
 						f = open("nyt.sent", "w+")
 						f.write("\n".join(concat(sents)).replace("''", '"'))
 						f.close()
